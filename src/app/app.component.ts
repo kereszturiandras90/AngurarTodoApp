2466a5f4d7b8ee40d7c160ffa8243467b1721007
LAPTOP-M24PTUQR\keres <kereszturiandras90@gmail.com>
2021-02-01 22:39:46 +0100

    jgggg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,17 @@ export class AppComponent {
   todos = [{label: 'Brink Milk', done: false, priority: 3}, 
   {label: 'clean room', done: true, priority: 1},
   {label: 'kill bill', done: false, priority: 2},];
+
+  addTodo(newTodoLabel: any) {
+    var newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done:false
+    };
+    this.todos.push(newTodo)
+  }
+
+  deleteTodo(todo: any){
+    this.todos = this.todos.filter(t => t.label !== todo.label);
+  }
 }
